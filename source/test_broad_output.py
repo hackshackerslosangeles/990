1257a4f03f5e17253f0055c6dab3fd19bca1b26d
danhillreports <danhillreports@gmail.com>
2016-03-09 09:03:00 -0800

    Add docstrings

--- source/test_broad_output.py
+++ source/test_broad_output.py
@@ -9,14 +9,21 @@ def main():
 
 
 def read_lines():
+    """Read each line of the OCR'd text file and append the line to a list."""
     with open("output990/0.txt", "r") as f:
         for line in f:
             lines.append(line.strip())
 
 
 def process_lines():
-    # if find potential address line, get name from line before
-    # and state from line after
+    """
+    Parse organization title and state abbreviation.
+
+    If line contains a potential address, get name from line before and state
+    from line after. Currently the method will only catch
+    the organization title "AGA" and the state "MD" from 990 entry
+    containing "AGA INSTITUTE" and "BETHESDA, MD".
+    """
     for (idx, line) in enumerate(lines):
         if pattern.match(line) and len(lines[idx - 1]) > 0:
             title_line = lines[idx - 1]
@@ -33,6 +40,7 @@ def process_lines():
 
 
 def format_request(title, state):
+    """Create URL to request to ProPublica 990 explorer API."""
     request = ("https://projects.propublica.org/nonprofits/api/v1/"
                "search.json?q={title}&state%5Bid%5D={state}"
                "&output=flat").format(title=title, state=state)
@@ -40,11 +48,13 @@ def format_request(title, state):
 
 
 def search_api(request):
+    """Send request to ProPublica 990 explorer API."""
     response = urllib2.urlopen(request)
     test_response(response)
 
 
 def test_response(response):
+    """Print EIN values present in ProPublica 990 API response."""
     data = json.loads(response.read())
 
     for f in data["filings"]:
@@ -53,5 +63,6 @@ def test_response(response):
 
 if __name__ == "__main__":
     lines = []
+    # potential address regex
     pattern = re.compile("\d+ [a-zA-z]+")
     main()
